251350f8ab3a29c431566138b4d62cd6a757c20e
GencoMattia <gencomattia@hotmail.it>
2024-06-17 10:39:56 +0200

    created ProductCard.vye component and added basic structure

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    data() {
+        return {
+        
+        };
+    }
+};
+</script>
+
+<template>
+    <article class="product-card">
+        <img src="" alt="">
+        <div class="product-info">
+            <p class="product-title">
+
+            </p>
+            <p class="product-original-title">
+
+            </p>
+            <p class="product-vote">
+
+            </p>
+            <p class="product-overview">
+
+            </p>
+        </div>
+    </article>
+</template>
+
+<style scoped>
+
+</style>
