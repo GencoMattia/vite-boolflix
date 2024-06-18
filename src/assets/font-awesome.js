ca9381d2ea1f20cd788d8a21f3fa5cc3aca2d351
GencoMattia <gencomattia@hotmail.it>
2024-06-18 11:22:20 +0200

    installed font-awesome, imported library to font-awesome.js file

---
+++ src/assets/font-awesome.js
@@ -0,0 +1,7 @@
+import { library } from "../../node_modules/@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "../../node_modules/@fortawesome/vue-fontawesome";
+import { fas } from "../../node_modules/@fortawesome/free-solid-svg-icons";
+
+library.add(fas);
+
+export { FontAwesomeIcon };
